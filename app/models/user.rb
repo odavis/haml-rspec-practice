12894d0b9d0c535f839615d8db8f86d09e3e9980
Orrett Davis <orrett.davis@gmail.com>
2014-04-23 15:29:09 -0400

    spec for downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :password, length: {minimum: 5}
   has_secure_password
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
